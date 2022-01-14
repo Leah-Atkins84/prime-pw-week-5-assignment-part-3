b0b10e0a249301dc3665a7f8fd6ce59dc5e643cb
Leah Atkins <leah.atkins84@gmail.com>
2022-01-13 20:42:29 -0600

    this will addToCollection function to assignment

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,2 +1,15 @@
 console.log('***** Music Collection *****')
 let collection = [];
+
+function addToCollection(title, artist, yearPublished){
+  // new object with above properties
+  let object = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
+
+  }//end Object
+  collection.push(object);
+  //return an object(title, artist, yearPublished)
+  return object;
+}
