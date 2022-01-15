2d78a3e597ba9e13007bdd10cac940c6c0efef30
Leah Atkins <leah.atkins84@gmail.com>
2022-01-15 14:11:01 -0600

    this will add album object and add albums to collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,13 +3,24 @@ let collection = [];
 
 function addToCollection(title, artist, yearPublished){
   // new object with above properties
-  let object = {
+  let album = {
     title: title,
     artist: artist,
     yearPublished: yearPublished,
-
   }//end Object
-  collection.push(object);
-  //return an object(title, artist, yearPublished)
-  return object;
+  //push album
+  collection.push(album);
+  //return object(title, artist, yearPublished)
+  return album;
 }
+//add 6 albums to collection
+
+addToCollection('Zaba', 'Glass Animals', 2014);
+console.log(collection);
+addToCollection('How to be a Human Being', 'Glass Animals', 2016);
+addToCollection('Dream Land', 'Glass Animals', 2020);
+addToCollection('Neon Jungle', 'CloZee',2020);
+addToCollection('Nouvelle Era', 'CloZee',2021);
+addToCollection('Back to Forever', 'Lissie', 2013);
+//log the collection array
+console.log(collection);
